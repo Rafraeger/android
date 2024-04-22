69bc0124235ba930a21ef930cd60f9dd25948ad3
Rafraeger <rafaime3@gmail.com>
2024-04-23 00:56:35 +0700

    Tugas 3

--- app/src/main/java/com/example/myapplication/Mainhome2.java
+++ app/src/main/java/com/example/myapplication/Mainhome2.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Mainhome2 extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class Mainhome2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainhome2);
+    }
+
+    public void page_login(View view) {
+        Intent intent = new Intent(Mainhome2.this,page_login.class);
+        startActivity(intent );
+
+    }
+
+    public void page_register(View view) {
+        Intent intent = new Intent(Mainhome2.this,page_register.class);
+        startActivity(intent );
+
     }
 }
