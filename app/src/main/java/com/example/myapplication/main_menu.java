74637442da44f90d3a925b8e6d60984f86bde0fd
Rafraeger <rafaime3@gmail.com>
2024-04-23 21:44:41 +0700

    Tugas 4

--- app/src/main/java/com/example/myapplication/main_menu.java
+++ app/src/main/java/com/example/myapplication/main_menu.java
@@ -4,12 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import androidx.drawerlayout.widget.DrawerLayout;
+import android.view.View;
+import android.widget.Button;
+import androidx.core.view.GravityCompat;
+
+
 public class main_menu extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private Button groupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        groupButton = findViewById(R.id.group_869);
+
+        groupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.END);
+            }
+        });
 
     }
 }
